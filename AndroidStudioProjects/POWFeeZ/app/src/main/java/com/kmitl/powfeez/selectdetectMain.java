d7aef5f899667020a8426426d400e2e4797f0644
fythatthepce <fythatthepce@gmail.com>
2017-09-23 15:33:48 +0700

    Start App

---
+++ AndroidStudioProjects/POWFeeZ/app/src/main/java/com/kmitl/powfeez/selectdetectMain.java
@@ -0,0 +1,13 @@
+package com.kmitl.powfeez;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class selectdetectMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selectdetect_main);
+    }
+}
