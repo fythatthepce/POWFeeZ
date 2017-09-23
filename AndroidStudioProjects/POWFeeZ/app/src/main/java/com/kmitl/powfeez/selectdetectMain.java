93de53287c911e2fea5fce0b2e55e757811d17ff
fythatthepce <fythatthepce@gmail.com>
2017-09-23 16:11:57 +0700

    Test push to git

--- AndroidStudioProjects/POWFeeZ/app/src/main/java/com/kmitl/powfeez/selectdetectMain.java
+++ AndroidStudioProjects/POWFeeZ/app/src/main/java/com/kmitl/powfeez/selectdetectMain.java
@@ -3,11 +3,11 @@ package com.kmitl.powfeez;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class selectdetectMain extends AppCompatActivity {
+public class selectdetectmain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selectdetect_main);
+        setContentView(R.layout.activity_selectdetectmain);
     }
 }
