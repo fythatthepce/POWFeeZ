468c207afa40c2a1f1036c5496819baa2f9da9ce
fythatthepce <fythatthepce@gmail.com>
2018-02-18 23:13:47 +0700

    Big update 2018

---
+++ AndroidStudioProjects/sound2/app/src/main/java/com/example/feez/sound2/camcv.java
@@ -0,0 +1,16 @@
+package com.example.feez.sound2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class camcv extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camcv);
+
+    }
+
+}
